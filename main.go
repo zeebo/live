026c0de87687d0ab26e0a322ec40c931a9e7ca0e
Jeff Wendling <jeff@spacemonkey.com>
2018-02-28 14:20:24 -0700

    simplify a bunch

--- main.go
+++ main.go
@@ -4,27 +4,24 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
-	"sync"
 	"time"
 )
 
 var (
-	dir           = flag.String("dir", "", "watched directory")
-	build         = flag.String("build", "", "build command")
-	run           = flag.String("run", "", "run command")
-	coalesceSleep = flag.Duration("coalesce", 100*time.Millisecond,
-		"how long to sleep accumulating events")
+	dir        = flag.String("dir", "", "watched directory")
+	build      = flag.String("build", "", "build command")
+	run        = flag.String("run", "", "run command")
+	quiescence = flag.Duration("quiescence", 100*time.Millisecond,
+		"quiescence period after a change before triggering build")
 
 	starting = make(chan struct{})
 	done     = make(chan struct{})
 	notify   = make(chan struct{})
 
-	mu            sync.Mutex
-	running       *exec.Cmd
-	running_stdin io.Closer
+	running      *exec.Cmd
+	running_done = make(chan struct{})
 )
 
 func main() {
@@ -35,104 +32,43 @@ func main() {
 		os.Exit(1)
 	}
 
-	// kick off a build/run
-	if err := attemptBuild(); err == nil {
-		attemptRun()
-	}
+	attempt()
 
 	go watch()
-	waiting_start := false // if we're waiting for a start
-	waiting_done := false  // if we're waiting for the build/run to be done
+
+	var (
+		timer   *time.Timer
+		waiting bool
+	)
 
 	for {
-		var notify_ch chan struct{}
-		if !waiting_done {
-			notify_ch = notify
+		var timer_ch <-chan time.Time
+		if timer != nil {
+			timer_ch = timer.C
 		}
 
 		select {
-		case <-notify_ch:
-			if waiting_start {
-				break
+		case <-timer_ch:
+			fmt.Println("--- quiescence reached")
+			go attempt()
+			timer = nil
+			waiting = false
+
+		case <-notify:
+			if !waiting {
+				fmt.Println("--- modification detected...")
+				waiting = true
 			}
-			waiting_start = true
-			go signaled()
-
-		case <-starting:
-			waiting_start = false
-			waiting_done = true
-
-		case <-done:
-			waiting_done = false
+			if timer != nil {
+				timer.Stop()
+			}
+			timer = time.NewTimer(*quiescence)
 		}
 	}
 }
 
-//
-// command launches the command in such a way that if its stdin is closed, the
-// child is killed.
-//
-
-func command(args string) (*exec.Cmd, io.Closer) {
-	pr, pw, err := os.Pipe()
-	if err != nil {
-		panic(err)
-	}
-
-	cmd := exec.Command("bash", "-c",
-		fmt.Sprintf("(%s)& (read)& wait -n; kill $(jobs -p)", args))
-	cmd.Stdin = pr
-
-	return cmd, pw
-}
-
-//
-// keep track of a running command
-//
-
-func set(cmd *exec.Cmd, stdin io.Closer) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	if running != nil {
-		fmt.Printf("--- killing old process pid: %d\n", running.Process.Pid)
-		running_stdin.Close()
-		running.Process.Wait()
-
-		running = nil
-		running_stdin = nil
-	}
-
-	if err := cmd.Start(); err != nil {
-		fmt.Printf("--- error starting process: %+v\n", err)
-	} else {
-		fmt.Printf("--- process started with pid: %d\n", cmd.Process.Pid)
-
-		running = cmd
-		running_stdin = stdin
-		go wait(cmd)
-	}
-}
-
-func wait(cmd *exec.Cmd) {
-	cmd.Wait()
-
-	mu.Lock()
-	defer mu.Unlock()
-
-	fmt.Printf("--- process exited with pid: %d\n", cmd.Process.Pid)
-	if cmd != running {
-		return
-	}
-	running = nil
-}
-
-//
-// watch sends on notify whenever a line comes in from fswatch
-//
-
 func watch() {
-	cmd, _ := command(fmt.Sprintf(`fswatch "%s"`, *dir))
+	cmd := exec.Command("fswatch", *dir)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		panic(err)
@@ -154,18 +90,7 @@ func watch() {
 	panic("fswatch exited unexpectedly")
 }
 
-//
-// signaled waits to start, and then attempts the build and run, sending on
-// done when it's done.
-//
-
-func signaled() {
-	fmt.Println("--- modification detected...")
-	time.Sleep(*coalesceSleep)
-
-	starting <- struct{}{}
-	defer func() { done <- struct{}{} }()
-
+func attempt() {
 	if err := attemptBuild(); err != nil {
 		return
 	}
@@ -178,7 +103,7 @@ func attemptBuild() (err error) {
 	}
 
 	fmt.Println("--- attempting build...")
-	cmd, _ := command(*build)
+	cmd := exec.Command(*build)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
@@ -190,8 +115,27 @@ func attemptRun() {
 	}
 
 	fmt.Println("--- attempting run...")
-	cmd, stdin := command(*run)
+	cmd := exec.Command(*run)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	set(cmd, stdin)
+
+	if running != nil {
+		fmt.Printf("--- killing old process pid: %d\n", running.Process.Pid)
+		running.Process.Kill()
+		running.Wait()
+		<-running_done
+		running = nil
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("--- error starting process: %+v\n", err)
+	} else {
+		fmt.Printf("--- process started with pid: %d\n", cmd.Process.Pid)
+		running = cmd
+		go func() {
+			cmd.Wait()
+			fmt.Printf("--- process exited with pid: %d\n", cmd.Process.Pid)
+			running_done <- struct{}{}
+		}()
+	}
 }
